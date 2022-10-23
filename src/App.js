d9233b249325c56d20ac55f230afb53a5222261b
Mitya <mitro_fan@inbox.ru>
2022-10-23 23:04:36 +0300

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
+import { useEffect } from "react";
 
 const Header = (props) => {
     const tg = window.Telegram.WebApp
